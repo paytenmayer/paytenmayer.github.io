933e6ede1600096932b62b6711cd71a99bcda1e5
Payten Mayer <paytenmayer066@gmail.com>
2024-03-05 17:16:07 +0000

    a

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -71,13 +71,21 @@ var init = function (window) {
         */
         game.checkCirclePosition = function(circle) {
               // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-  if (circle.x > canvas.width) {
-    circle.x = 0;
-  }
+            if (circle.x > canvas.width) {
+                circle.x = 0;
+            }
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
-                circle.x = 0;
+            if ( circle.x < 0) {
+                circle.x = canvas.width;
+            }
+
+            if ( circle.y < 0) {
+                circle.y = canvas.height;
+            }
+
+            if ( circle.y > canvas.height) {
+                circle.y = 0;
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
